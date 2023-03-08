86bfc5527ddaf9b91aef078f24a2b3c2fb59a54c
Xiangyu Sun <xiangyu.sun@glovoapp.com>
2023-03-08 16:22:29 +0100

    adding function that checks the status

--- Sources/ShangdianKit/Store.swift
+++ Sources/ShangdianKit/Store.swift
@@ -177,6 +177,54 @@ public class Store: StoreProtocol {
       print("Failed product request: \(error)")
     }
   }
+  
+  @MainActor
+  public func updateSubscriptionStatus() async throws -> (Product.SubscriptionInfo.Status?, Product?)? {
+    // This app has only one subscription group so products in the subscriptions
+    // array all belong to the same group. The statuses returned by
+    // `product.subscription.status` apply to the entire subscription group.
+    guard
+      let product = subscriptions.first,
+      let statuses = try await product.subscription?.status else
+    {
+      return nil
+    }
+    
+    var highestStatus: Product.SubscriptionInfo.Status? = nil
+    var highestProduct: Product? = nil
+    
+    // Iterate through `statuses` for this subscription group and find
+    // the `Status` with the highest level of service which isn't
+    // expired or revoked.
+    for status in statuses {
+      switch status.state {
+      case .expired, .revoked:
+        continue
+      default:
+        let renewalInfo = try checkVerified(status.renewalInfo)
+        
+        guard let newSubscription = subscriptions.first(where: { $0.id == renewalInfo.currentProductID }) else {
+          continue
+        }
+        
+        guard let currentProduct = highestProduct else {
+          highestStatus = status
+          highestProduct = newSubscription
+          continue
+        }
+        
+        let highestTier = tier(for: currentProduct.id)
+        let newTier = tier(for: renewalInfo.currentProductID)
+        
+        if newTier > highestTier {
+          highestStatus = status
+          highestProduct = newSubscription
+        }
+      }
+    }
+    
+    return (highestStatus, highestProduct)
+  }
 
   @MainActor
   func updatePurchasedIdentifiers(_ transaction: Transaction) async {
