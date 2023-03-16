5dfd91f472f7da8e865f2baecd8dd0d4a3df750b
Xiangyu Sun <xiangyu.sun@glovoapp.com>
2023-03-16 23:17:14 +0100

    adding UI

---
+++ Sources/ShangdianKit/UI/PurchaseModel.swift
@@ -0,0 +1,26 @@
+import StoreKit
+import SwiftUI
+
+public final class PurchaseModel: ObservableObject {
+
+  @Published
+  var errorTitle: String? = nil
+  @Published
+  var isPurchased: Bool = false
+  
+  @MainActor
+  func buy(product: Product, store: Store) async {
+    do {
+      if try await store.purchase(product) != nil {
+        withAnimation {
+          isPurchased = true
+        }
+      }
+    } catch StoreError.failedVerification {
+      errorTitle = NSLocalizedString("Your purchase could not be verified by the App Store.", comment: "")
+    } catch {
+      errorTitle = error.localizedDescription
+    }
+  }
+  
+}
